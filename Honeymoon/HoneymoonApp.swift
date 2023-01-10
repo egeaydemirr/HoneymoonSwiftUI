bbb1d2e07a470ac36c1ab7fa57279c141e7f0df2
Ege Aydemir <egeaydemirr97@gmail.com>
2023-01-10 20:01:27 +0300

    First Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Ege Aydemir on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
