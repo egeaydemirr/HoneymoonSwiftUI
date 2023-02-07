a810ccd919f4bb623274e238cc6543565e26fbe7
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-07 18:26:04 +0300

    FooterView

---
+++ Honeymoon/View/FooterView.swift
@@ -0,0 +1,45 @@
+//
+//  FooterView.swift
+//  Honeymoon
+//
+//  Created by Ege Aydemir on 7.02.2023.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "xmark.circle")
+                .font(.system(size: 42, weight: .light))
+            Spacer()
+            
+            Button(action:{
+                //Action:
+                print("Success!")
+            }){
+                Text("Book Destination".uppercased())
+                    .font(.system(.subheadline, design: .rounded))
+                    .fontWeight(.heavy)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 12)
+                    .accentColor(.pink)
+                    .background(
+                        Capsule().stroke(.pink, lineWidth:2)
+                    )
+            }
+            
+            Spacer()
+            Image(systemName: "heart.circle")
+                .font(.system(size: 42, weight: .light))
+        }
+        .padding()
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.fixed(width: 375, height: 80))
+    }
+}
