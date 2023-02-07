c6f613ed5b4971b34cb93c4c0443380505fe158a
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-07 16:40:55 +0300

    ModelFiles

---
+++ Honeymoon/HoneymoonData.swift
@@ -0,0 +1,8 @@
+//
+//  HoneymoonData.swift
+//  Honeymoon
+//
+//  Created by Ege Aydemir on 7.02.2023.
+//
+
+import Foundation
---
+++ Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Ege Aydemir on 7.02.2023.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
