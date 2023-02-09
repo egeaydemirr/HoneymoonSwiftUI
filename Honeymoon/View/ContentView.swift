544906c47be3cec5a46d722f642c5fb4ed6e5c02
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-10 02:06:59 +0300

    Binding InfoView

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -11,9 +11,10 @@ struct ContentView: View {
     // MARK: - Properties
     @State var showAlert: Bool = false
     @State var showGuide: Bool = false
+    @State var showInfo: Bool = false
     var body: some View {
         VStack {
-            HeaderView(showGuideView: $showGuide)
+            HeaderView(showGuideView: $showGuide, showInfoView: $showInfo)
             Spacer()
             CardView(honeymoon: honeymoonData[2])
                 //Add padding to the cards later on
--- Honeymoon/View/HeaderView.swift
+++ Honeymoon/View/HeaderView.swift
@@ -11,20 +11,23 @@ struct HeaderView: View {
     // MARK: - Properties
     
     @Binding var showGuideView: Bool
+    @Binding var showInfoView:Bool
     
     var body: some View {
         HStack {
             Button(action: {
                 //:Action
-                print("Information")
+//                print("Information")
+                self.showInfoView.toggle()
             }){
                 Image(systemName: "info.circle")
                     .font(.system(size: 24, weight: .regular))
             }
             .accentColor(.primary)
-            .sheet(isPresented: $showGuideView){
-                GuideView()
+            .sheet(isPresented: $showInfoView){
+                InfoView()
             }
+            
             
             Spacer()
             
@@ -43,6 +46,9 @@ struct HeaderView: View {
                     .font(.system(size:24, weight: .regular))
             }
             .accentColor(.primary)
+            .sheet(isPresented: $showGuideView){
+                GuideView()
+            }
         }
         .padding()
     }
@@ -50,8 +56,9 @@ struct HeaderView: View {
 
 struct HeaderView_Previews: PreviewProvider {
     @State static var showGuide: Bool = false
+    @State static var showInfo: Bool = false
     static var previews: some View {
-        HeaderView(showGuideView: $showGuide)
+        HeaderView(showGuideView: $showGuide, showInfoView: $showInfo)
             .previewLayout(.fixed(width: 375, height: 80))
     }
 }
