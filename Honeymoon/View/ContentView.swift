e854c4028d78300c7168dd2a30191a9d41593d13
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-10 16:54:27 +0300

    *enum DragStates: (pressing, dragging, inactive)

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -29,13 +29,45 @@ struct ContentView: View {
         }
         return index == 0
     }
+    // MARK: - Drag States
+    enum DragState {
+        case inactive
+        case pressing
+        case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self {
+            case .inactive, .pressing:
+                return .zero
+            case .dragging(let translation):
+                return translation
+            }
+        }
+        var isDragging: Bool {
+            switch self {
+            case .dragging:
+                return true
+            case .pressing, .inactive:
+                return false
+            }
+        }
+        var isPressing: Bool {
+            switch self {
+            case .pressing, .dragging:
+                    return true
+            case .inactive:
+                return false
+            }
+        }
+    }
+    
     
     var body: some View {
         VStack {
             // MARK: - Header
             HeaderView(showGuideView: $showGuide, showInfoView: $showInfo)
             Spacer()
-            // MARK: - Card
+            // MARK: - Cards
             ZStack{
                 ForEach(cardViews) {cardView in
                     cardView
