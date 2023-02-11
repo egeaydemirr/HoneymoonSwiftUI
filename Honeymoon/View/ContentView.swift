c8dfe6e8a9c8ac8fcc6ec9c66ef865d7f5833744
Ege Aydemir <egeaydemirr97@gmail.com>
2023-02-11 16:44:21 +0300

    Implementation Symbol Mod

--- Honeymoon/View/ContentView.swift
+++ Honeymoon/View/ContentView.swift
@@ -79,9 +79,7 @@ struct ContentView: View {
                             ZStack{
                                 // MARK: - Xmark
                                 Image(systemName: "x.circle")
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 128))
-                                    .shadow(color:Color(UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)), radius: 12, x: 0, y: 0)
+                                    .modifier(SymbolModifier())
                             }
                         )
                         .offset(x: self.isTopCard(cardView: cardView) ?  self.dragState.translation.width : 0, y: self.isTopCard(cardView: cardView) ?  self.dragState.translation.height : 0)
